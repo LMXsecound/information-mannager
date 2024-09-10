9ba4b2e2aa752a24c6bb268963046aa7247ff850
敏轩 劳 <3301085697@qq.com>
2024-09-10 22:41:41 +0800

    1

--- 源1.cpp
+++ 源1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
+#include <string>
 using namespace std;
-
+#define MAX 1000
 void showMenu()
 {
 	cout << "***************************" << endl;
@@ -15,6 +16,20 @@ void showMenu()
 
 }
 
+struct Person
+{
+	string m_Name; //姓名
+	int m_Sex; //性别：1男 2女
+	int m_Age; //年龄
+	string m_Phone; //电话
+	string m_Addr; //住址
+};
+
+struct Addressbooks
+{
+	struct Person personArray[MAX]; //通讯录中保存的联系人数组
+	int m_Size; //通讯录中人员个数
+};
 
 int main()
 {
