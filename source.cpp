4b22996ca068f341bad317e0e8810ee0a62baf77
敏轩 劳 <3301085697@qq.com>
2024-09-11 13:00:13 +0800

    1

--- source.cpp
+++ source.cpp
@@ -30,17 +30,77 @@ void showMenu()
 struct Person
 {
 	bool m_Sex; // 0->male, 1->female
-	int m_Age; // ����
-	string m_Name; // ����
-	string m_Phone; // �绰
-	string m_Addr; // ��ַ
+	int m_Age; //年龄
+	string m_Name; //姓名
+	string m_Phone; //电话
+	string m_Addr;  //住址
 };
 
 struct Addressbooks
 {
-	Person personArray[MAX]; // ͨѶ¼�б������ϵ������
-	int m_Size; // ͨѶ¼����Ա����
+	Person personArray[MAX]; //通讯录中保存的联系人数组
+	int m_Size; //通讯录中人员个数
 };
+
+void addPerson(Addressbooks abs)
+{
+	//判断电话本是否满了
+	if (abs.m_Size == MAX)
+	{
+		cout << "通讯录已满，无法添加" << endl;
+		return;
+	}
+	else
+	{
+		//姓名
+		string name;
+		cout << "请输入姓名：" << endl;
+		cin >> name;
+		//abs->personArray[abs->m_Size].m_Name = name;
+
+		cout << "请输入性别：" << endl;
+		cout << "1 -- 男" << endl;
+		cout << "2 -- 女" << endl;
+
+		//性别
+		int sex = 0;
+		while (true)
+		{
+			cin >> sex;
+			if (sex == 1 || sex == 2)
+			{
+				/*abs->personArray[abs->m_Size].m_Sex = sex;*/
+				break;
+			}
+			cout << "输入有误，请重新输入";
+		}
+
+		//年龄
+		cout << "请输入年龄：" << endl;
+		int age = 0;
+		cin >> age;
+		/*abs->personArray[abs->m_Size].m_Age = age;*/
+
+		//联系电话
+		cout << "请输入联系电话：" << endl;
+		string phone = "";
+		cin >> phone;
+		/*abs->personArray[abs->m_Size].m_Phone = phone;*/
+
+		//家庭住址
+		cout << "请输入家庭住址：" << endl;
+		string address;
+		cin >> address;
+		/*abs->personArray[abs->m_Size].m_Addr = address;*/
+
+		//更新通讯录人数
+		/*abs->m_Size++;*/
+
+		cout << "添加成功" << endl;
+		system("pause");
+		system("cls");
+	}
+}
 
 int main()
 {
