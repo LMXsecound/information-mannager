4262332e887559aea8e9ff0e136689d69deb2a8b
敏轩 劳 <3301085697@qq.com>
2024-09-11 22:39:08 +0800

    1

--- source.cpp
+++ source.cpp
@@ -59,8 +59,8 @@ void addPerson(Addressbooks &abs)
 		abs.personArray[abs.m_Size].m_Name = name;
 
 		cout << "请输入性别：" << endl;
-		cout << "1 -- 男" << endl;
-		cout << "2 -- 女" << endl;
+		cout << "0 -- 男" << endl;
+		cout << "1 -- 女" << endl;
 
 		//性别
 		int sex = 0;
@@ -101,7 +101,28 @@ void addPerson(Addressbooks &abs)
 		system("cls");
 	}
 }
+void showPerson(Addressbooks &abs)
+{
+	if (abs.m_Size == 0)
+	{
+		cout << "当前记录为空" << endl;
+	}
+	else
+	{
+		for (int i = 0; i < abs.m_Size; i++)
+		{
+			cout << "姓名：" << abs.personArray[i].m_Name << "\t";
+			cout << "性别：" << (abs.personArray[i].m_Sex == 1 ? "男" : "女") << "\t";
+			cout << "年龄：" << abs.personArray[i].m_Age << "\t";
+			cout << "电话：" << abs.personArray[i].m_Phone << "\t";
+			cout << "住址：" << abs.personArray[i].m_Addr << endl;
+		}
+	}
 
+	system("pause");
+	system("cls");
+
+}
 int main()
 {
 	int select = 0;
@@ -123,6 +144,7 @@ int main()
 			break;
 
 		case SHOW:
+			showPerson(abs);
 			break;
 
 		case DEL:
